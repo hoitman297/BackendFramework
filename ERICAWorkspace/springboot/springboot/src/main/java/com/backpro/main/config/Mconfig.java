b2b38858d0436f8afc6d6dedb333edca8f636e29
DESKTOP-VS3GEDC\user1 <will69378@gmail.com>
2026-04-29 12:18:10 +0900

    연결 밑작업

---
+++ ERICAWorkspace/springboot/springboot/src/main/java/com/backpro/main/config/Mconfig.java
@@ -0,0 +1,25 @@
+package com.backpro.main.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.*;
+
+@Configuration
+public class Mconfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:3000"); // 🔥 React 주소
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
---
+++ ERICAWorkspace/springboot/springboot/src/main/java/com/backpro/main/repository/MainRepository.java
@@ -0,0 +1,9 @@
+package com.backpro.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.backpro.main.model.vo.Main;
+
+public interface MainRepository extends JpaRepository<Main, Long>{
+
+}
